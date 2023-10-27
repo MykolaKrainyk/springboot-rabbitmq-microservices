ccab79e7f9040d3a306c85ffc3a4c9f7c74c1faf
Mykola Krainyk <mykola_krainyk@epam.com>
2023-10-27 17:16:54 +0300

    add message converter and rabbit template config in order service

--- order-service/src/main/java/com/example/orderservice/config/RabbitMQConfig.java
+++ order-service/src/main/java/com/example/orderservice/config/RabbitMQConfig.java
@@ -1,9 +1,14 @@
 package com.example.orderservice.config;
 
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -37,6 +42,18 @@ public class RabbitMQConfig {
                 .to(exchange())
                 .with(orderRoutingKey);
     }
+
     // message converter
+    @Bean
+    public MessageConverter converter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
     // configure RabbitTemplate
+    @Bean
+    public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(converter());
+        return rabbitTemplate;
+    }
 }
