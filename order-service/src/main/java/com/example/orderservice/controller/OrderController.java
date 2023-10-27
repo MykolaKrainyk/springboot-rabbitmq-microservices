747154d30554eeee0a68f8a61580217d8c46dc71
Mykola Krainyk <mykola_krainyk@epam.com>
2023-10-27 18:09:09 +0300

    add OrderController and publish event to RabbitMQ

---
+++ order-service/src/main/java/com/example/orderservice/controller/OrderController.java
@@ -0,0 +1,38 @@
+package com.example.orderservice.controller;
+
+import com.example.orderservice.dto.Order;
+import com.example.orderservice.dto.OrderEvent;
+import com.example.orderservice.publisher.OrderProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1")
+public class OrderController {
+
+    private OrderProducer orderProducer;
+
+    @Autowired
+    public OrderController(OrderProducer orderProducer) {
+        this.orderProducer = orderProducer;
+    }
+
+    @PostMapping("/orders")
+    public String placeOrder(@RequestBody Order order) {
+        order.setOrderId(UUID.randomUUID().toString());
+
+        OrderEvent orderEvent = new OrderEvent();
+        orderEvent.setStatus("PENDING");
+        orderEvent.setMessage("Order is in pending status");
+        orderEvent.setOrder(order);
+
+        orderProducer.sendMessage(orderEvent);
+
+        return "Order sent to the RabbitMQ ...";
+    }
+}
